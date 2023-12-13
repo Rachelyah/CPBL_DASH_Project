1bcf727ff71373c36ac903ce258aba1afabb4ff9
Rachel Yeh <rachelyeh.tw@gmail.com>
2023-12-13 13:58:17 +0000

    modify

--- dash_file/dash_app3.py
+++ dash_file/dash_app3.py
@@ -5,7 +5,8 @@ from . import cpbl_datasource
 
 current_data = cpbl_datasource.lastest_datetime_data()
 
-current_df = pd.DataFrame(current_data,columns=['年份', '所屬球隊', '球員編號', '球員姓名', '出場數', '先發次數', '中繼次數', '勝場數', '敗場數', '救援成功', '中繼成功', '有效局數', '面對打者數', '被安打數', '被全壘打數', '保送數', '三振數', '自責分', '投打習慣', '背號', '身高體重', '生日', '照片網址', '奪三振率', '防禦率'])
+current_df = pd.DataFrame(current_data,
+                          columns=['年份', '所屬球隊', '球員編號', '球員姓名', '出場數', '先發次數', '中繼次數', '勝場數', '敗場數', '救援成功', '中繼成功', '有效局數', '面對打者數', '被安打數', '被全壘打數', '保送數', '三振數', '自責分', '投打習慣', '背號', '身高體重', '生日', '照片網址', '奪三振率', '防禦率'])
 
 dash3 = Dash(requests_pathname_prefix="/dash/app3/", external_stylesheets=[dbc.themes.BOOTSTRAP])
 dash3.title='中華職棒查詢dash3'
@@ -44,9 +45,9 @@ dash3.layout = html.Div(
             html.Div([
                 html.Div([
                     dash_table.DataTable(
-                        id='table',
+                        id='main_table',
                         data=current_df.to_dict('records'),
-                        columns=[{'id':column,'name':column} for column in current_df.columns],
+                        columns=[{'id': column, 'name': column} for column in current_df.columns],
                         page_size=20,
                         style_table={'height': '300px', 'overflowY': 'auto'},
                         fixed_rows={'headers': True},
@@ -126,13 +127,14 @@ dash3.layout = html.Div(
 
 #回傳查詢的確定按鈕被按了幾次
 @callback(
-    [Output('table','data'), Output('table', 'column'), Output('table', 'selected_rows')],
+    [Output('main_table','data'),Output('main_table', 'column'),Output('main_table','selected_rows')],
     [Input('submit-val','n_clicks')],
     [State('input_value','value')]
 )
 def clickBtn(n_clicks:None | int, inputValue:str):
     global current_df
     if n_clicks is not None:
+        print('clickbtn判斷')
         #呼叫datasource的搜尋方法，傳出list[tuple]
         searchData:list[tuple] = cpbl_datasource.search_sitename(inputValue)
         current_df = pd.DataFrame(searchData,columns=['年份', '所屬球隊', '球員編號', '球員姓名', '出場數', '先發次數', '中繼次數', '勝場數', '敗場數', '救援成功', '中繼成功', '有效局數', '面對打者數', '被安打數', '被全壘打數', '保送數', '三振數', '自責分', '投打習慣', '背號', '身高體重', '生日', '照片網址', '奪三振率', '防禦率'])
@@ -144,23 +146,22 @@ def clickBtn(n_clicks:None | int, inputValue:str):
     
     
     #當clickBtn is None -> 「確定」按鈕沒被按下，網頁剛啟動時
-    else:
-        print('第一次啟動')
-        current_data = cpbl_datasource.lastest_datetime_data()
-        current_df = pd.DataFrame(current_data,columns=['年份', '所屬球隊', '球員編號', '球員姓名', '出場數', '先發次數', '中繼次數', '勝場數', '敗場數', '救援成功', '中繼成功', '有效局數', '面對打者數', '被安打數', '被全壘打數', '保送數', '三振數', '自責分', '投打習慣', '背號', '身高體重', '生日', '照片網址', '奪三振率', '防禦率'])
-        #current_df = current_df.reset_index()
-        #current_df['站點名稱'] = current_df['站點名稱'].map(lambda name:name[11:])
+    
+    print('第一次啟動')
+    current_data = cpbl_datasource.lastest_datetime_data()
+    current_df = pd.DataFrame(current_data,columns=['年份', '所屬球隊', '球員編號', '球員姓名', '出場數', '先發次數', '中繼次數', '勝場數', '敗場數', '救援成功', '中繼成功', '有效局數', '面對打者數', '被安打數', '被全壘打數', '保送數', '三振數', '自責分', '投打習慣', '背號', '身高體重', '生日', '照片網址', '奪三振率', '防禦率'])
 
-        return current_df.to_dict('records'), [{'id':column,'name':column} for column in current_df.columns],[]   
+    return current_df.to_dict('records'), [{'id':column,'name':column} for column in current_df.columns],[]   
 
 
 @callback(
     Output('showMessage','children'),
-    Input('table','selected_rows')
+    Input('main_table','selected_rows')
 )
 
 #抓出選擇的欄位內容
 def selectedRow(selected_rows:list[int]): #傳入list[裡面放int]
+        global current_df
         #取得一個站點，series
         #def可以取得py檔的文件變數
         if len(selected_rows) != 0:
